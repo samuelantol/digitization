e73176ad4d8a1674f6d47b2e8e25cf05da5b0d4d
Mgr. art. Samuel Antol <antols@sng.local>
2023-02-09 17:17:29 +0100

    slideshow not working

--- src/components/Flipper.vue
+++ src/components/Flipper.vue
@@ -23,6 +23,30 @@
         </div>
 
     </div>
+
+    <div class="container">
+        <div v-for="image in imagesLarge">
+            <div class="mySlides">
+                <div class="numbertext">1 / 6</div>
+                <img :src="image.src" style="width:100%">
+            </div>
+        </div>
+            
+        <a class="prev" @click="plusSlides(-1)">❮</a>
+        <a class="next" @click="plusSlides(1)">❯</a>
+
+        <!-- <div class="caption-container">
+            <p id="caption"></p>
+        </div> -->
+
+        <div class="row">
+            <div v-for="image in imagesSmall">
+                <div class="column">
+                    <img class="demo cursor" :class="{ active: isSelectedImage(image.index) }" :src="image.src" style="width:100%" @click="currentSlide(image.index)" alt="The Woods">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -40,6 +64,71 @@
         } else {
             return false;
         }
+    }
+
+    function isSelectedImage (index) {
+        if (selectedSlide.value === index) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //
+
+    const imagesSmall = ref ([
+        {index: 0, src: "https://www.w3schools.com/howto/img_woods.jpg"},
+        {index: 1, src: "https://www.w3schools.com/howto/img_5terre.jpg"},
+        {index: 2, src: "https://www.w3schools.com/howto/img_mountains.jpg"},
+        {index: 3, src: "https://www.w3schools.com/howto/img_lights.jpg"},
+        {index: 4, src: "https://www.w3schools.com/howto/img_nature.jpg"},
+        {index: 5, src: "https://www.w3schools.com/howto/img_snow.jpg"}
+    ]);
+    const imagesLarge = ref ([
+        {index: 0, src: "https://www.w3schools.com/howto/img_woods_wide.jpg"},
+        {index: 1, src: "https://www.w3schools.com/howto/img_5terre_wide.jpg"},
+        {index: 2, src: "https://www.w3schools.com/howto/img_mountains_wide.jpg"},
+        {index: 3, src: "https://www.w3schools.com/howto/img_lights_wide.jpg"},
+        {index: 4, src: "https://www.w3schools.com/howto/img_nature_wide.jpg"},
+        {index: 5, src: "https://www.w3schools.com/howto/img_snow_wide.jpg"}
+    ]);
+
+    const selectedSlide = ref(0);
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = imagesLarge.value; //document.getElementsByClassName("mySlides");
+        let dots = imagesSmall.value; //document.getElementsByClassName("demo");
+        //let captionText = document.getElementById("caption");
+
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+
+        // for (i = 0; i < slides.length; i++) {
+        //     slides[i].style.display = "none";
+        // }
+        // for (i = 0; i < dots.length; i++) {
+        //     dots[i].className = dots[i].className.replace(" active", "");
+        // }
+
+        // slides[slideIndex-1].style.display = "block";
+        //dots[slideIndex-1].className += " active";
+        //captionText.innerHTML = dots[slideIndex-1].alt;
     }
 </script>
 
@@ -71,5 +160,98 @@
 }
 .selected {
     color: #000000;
+}
+
+
+
+* {
+  box-sizing: border-box;
+}
+
+img {
+  vertical-align: middle;
+}
+
+/* Position the image container (needed to position the left and right arrows) */
+.container {
+  position: relative;
+}
+
+/* Hide the images by default */
+.mySlides {
+  display: none;
+}
+
+/* Add a pointer when hovering over the thumbnail images */
+.cursor {
+  cursor: pointer;
+}
+
+/* Next & previous buttons */
+.prev,
+.next {
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 16px;
+  margin-top: -50px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover,
+.next:hover {
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* Container for image text */
+.caption-container {
+  text-align: center;
+  background-color: #222;
+  padding: 2px 16px;
+  color: white;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Six columns side by side */
+.column {
+  float: left;
+  width: 16.66%;
+}
+
+/* Add a transparency effect for thumnbail images */
+.demo {
+  opacity: 0.6;
+}
+
+.active,
+.demo:hover {
+  opacity: 1;
 }
 </style>
